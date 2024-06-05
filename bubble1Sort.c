6bec44158126603785d078d841c0a39f10229463
Senuri1234 <158792381+Senuri1234@users.noreply.github.com>
2024-06-05 09:55:05 +0530

    Add files via upload

---
+++ bubble1Sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to perform Optimized Bubble Sort
+void bubbleSort(int arr[], int n) {
+	int i,j;
+    for ( i = 0; i < n-1; i++) {
+        bool swapped = false;
+        for ( j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        // If no two elements were swapped by inner loop, then break
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+// Function to print an array
+int i,j;
+void printArray(int arr[], int size) {
+    for ( i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
+
